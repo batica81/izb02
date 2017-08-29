6d224d99a989fbef7999acb979274187adbdfff9
batica81 <batica@gmail.com>
2017-08-29 23:52:58 +0200

    test get

---
+++ README.md
@@ -0,0 +1,6 @@
+izb02
+=====
+
+A Symfony project created on August 29, 2017, 9:26 pm.
+
+Druga iteracija aplikacije za IzbIS
---
+++ src/AppBundle/Controller/TestUserController.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Voja
+ * Date: 29-Aug-17
+ * Time: 23:46
+ */
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\View\View;
+use AppBundle\Entity\TestUser;
+
+
+class TestUserController extends FOSRestController
+{
+    /**
+     * @Rest\Get("/user")
+     */
+    public function getAction()
+    {
+        $restresult = $this->getDoctrine()->getRepository('AppBundle:TestUser')->findAll();
+        if ($restresult === null) {
+            return new View("there are no users exist", Response::HTTP_NOT_FOUND);
+        }
+        return $restresult;
+    }
+}
+
