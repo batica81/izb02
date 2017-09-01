45d9b9539d80daeb1d7ba64fe4a2610fb6b5a2c6
batica81 <batica@gmail.com>
2017-09-01 17:34:59 +0200

    zavrsen logout js

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,11 +9,12 @@ use Symfony\Component\HttpFoundation\Request;
 use FOS\RestBundle\View\View;
 use Symfony\Component\HttpFoundation\Response;
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/admin", name="adminhomepage")
      */
     public function indexAction(Request $request)
     {
@@ -21,7 +22,40 @@ class DefaultController extends Controller
 //        return $this->render('default/index.html.twig', [
 //            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
 //        ]);
-        return new View("there are no users exist", Response::HTTP_NOT_FOUND);
+//        return new View("there are no users exist", Response::HTTP_NOT_FOUND);
+//        return new View("Hello", Response::HTTP_OK);
 
+
+        return new Response('<html><body>Admin page!<br><a href="/logout">Logout</a></body></html>');
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logoutAction(Request $request)
+    {
+        // replace this example code with whatever you need
+//        return $this->render('default/index.html.twig', [
+//            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+//        ]);
+//        return new View("there are no users exist", Response::HTTP_NOT_FOUND);
+//        return new View("Bye Bye!", Response::HTTP_UNAUTHORIZED);
+//        return new Response('<html><body>Bye Bye!</body></html>', Response::HTTP_UNAUTHORIZED);
+
+        $response = new RedirectResponse('/admin',302,['Authorization' => 'Basic bG9nb3V0OmxvZ291dA==']);
+
+//        $response->headers->set('Authorization', 'Basic bG9nb3V0OmxvZ291dA==');
+
+        $response->headers->set('Authorization', 'Basic bG9nb3V0OmxvZ291dA==');
+
+        return $response;
+//        $request = Request::create(
+//            '/hello-world',
+//            'GET',
+//            array('name' => 'Fabien')
+//        );
+//
+//        $request::send();
+//        return new Response('<html><body>Admin page!</body></html>');
     }
 }
