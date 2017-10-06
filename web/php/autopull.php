430815ebd769fd1903b5b07ce72607b211386f37
Aleksandar Krajnalic2 <akrajnalic@aleksandarkrajnalic.com>
2017-10-06 11:27:14 +0200

    autopull za outerspace

---
+++ web/php/autopull.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: voja
+ * Date: 6.10.17.
+ * Time: 11.26
+ */
+
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $data = json_decode(file_get_contents('php://input'), true);
+    if ($data["repository"]["id"] == 101808407) {
+        echo shell_exec("git pull");
+    }
+}
+//TODO: hmac check for autopull
+//TODO: composer, npm and gulp tasks after pull
